0c99a8f9922fd51e6529e3692dc9218bd0aa6368
Angie Verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-05-05 11:13:57 -0500

    Ejemplo3 dart estructuras secuenciales

---
+++ trim1/programacion/dart_adso/ejemplos_dart.dart/ejemplos1_dart/bin/ejemplo3.dart
@@ -0,0 +1,12 @@
+//ESTRUCTURAS SECUENCIASLES 
+
+void main(List<String> args) {
+  //DEFINICION DE VARIABLES
+  double base = 10;
+  double altura = 5;
+  double area;
+
+  //PROCESO 
+  area = (base * altura) / 2;
+  print("El área del triángulo es: $area");
+}
