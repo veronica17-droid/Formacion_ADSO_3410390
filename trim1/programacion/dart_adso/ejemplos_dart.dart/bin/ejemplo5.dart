461eeb2699663c69736250b2c1feed5635517ec0
Angie Verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-05-12 07:24:10 -0500

    Correccion ejemplo 5 dart

--- trim1/programacion/dart_adso/ejemplos_dart.dart/bin/ejemplo5.dart
+++ trim1/programacion/dart_adso/ejemplos_dart.dart/bin/ejemplo5.dart
@@ -1,6 +1,6 @@
 //OPERADORES
 
-// Operador AND 
+// Operador AND
 import 'dart:io';
 
 void main(List<String> args) {
@@ -9,50 +9,52 @@ void main(List<String> args) {
 
   //Condicion: para ingresar. Ser mayor de edad y tener tieneCarnet
 
-  if (tieneCarnet == true && edad >= 18){
-    print("Bienvenido al centro de formación");
-  }else{
-    print("No se cumple con los requisitos de acceso");
+  if (tieneCarnet == true && edad >= 18) {
+    stdout.write("Bienvenido al centro de formación");
+  } else {
+    stdout.write("No se cumple con los requisitos de acceso");
   }
 
-  //Operador OR 
-    bool esFestivo = true;
-    bool esSabado = false;
-    //Condicion: si es sabado o festivo no hay formación
-    if(esFestivo==true || esSabado ==true){
-      print("No hay formación");
-    }
+  //Operador OR
+  bool esFestivo = true;
+  bool esSabado = false;
+  //Condicion: si es sabado o festivo no hay formación
+  if (esFestivo == true || esSabado == true) {
+    stdout.write("No hay formación");
+  }
 
-  //Operador NOT 
-    bool sistemaCaido = true; 
-    if (!sistemaCaido){
-      print("El sistema no está caido");
-    }else{
-      print("El sistema está caido");
-    }
+  //Operador NOT
+  bool sistemaCaido = true;
+  // ignore: dead_code
+  if (!sistemaCaido){
+    stdout.write("El sistema no está caido");
+  } else {
+    stdout.write("El sistema está caido");
+  }
 
-    //Operadores Combinados
-    //Aprueba si (entregó y sacó más de 3.5)
-    //O tiene una excusa válida y puede presentar de nuevo el examen
-    double nota = 3.4;
-    bool entrego = true;
-    bool excusaValida = false;
+  //Operadores Combinados
+  //Aprueba si (entregó y sacó más de 3.5)
+  //O tiene una excusa válida y puede presentar de nuevo el examen
+  double nota = 3.4;
+  bool entrego = true;
+  bool excusaValida = false;
 
-    if ((entrego ==true && nota >=4) || excusaValida == true){
-      if(entrego==true){ //si entregó el examen
-        if(nota>=3.5){
-          print("Aprueba el examen");
-        }else{
-          print("No aprueba el examen");
-        }
-      }else{
-        if(excusaValida ==true){
-          print("puede presentar el examen en otra fecha");
-        }else{
-          print("No puede presentar examen");
-        }
+  if ((entrego == true && nota >= 4) || excusaValida == true) {
+    if (entrego == true) {
+      //si entregó el examen
+      if (nota >= 3.5) {
+        print("Aprueba el examen");
+      } else {
+        print("No aprueba el examen");
       }
-    }else{
-      print("No aprueba ni puede presentar el examen");
+    } else {
+      if (excusaValida == true) {
+        print("puede presentar el examen en otra fecha");
+      } else {
+        print("No puede presentar examen");
+      }
     }
+  } else {
+    print("No aprueba ni puede presentar el examen");
+  }
 }
