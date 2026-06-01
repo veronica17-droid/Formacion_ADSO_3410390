cfcab961ab5ca8ee460ea8e4750b3f256531ded5
Angie Verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-06-01 08:15:10 -0500

    git

---
+++ trim1/programacion/dart_adso/ejeCondicionalesComplementarios/condicionales simples/bin/01.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+  //DEFINICIÓN DE VARIABLES
+  print("--Gestión de auxilios de transporte CPIC--");
+}
---
+++ trim1/programacion/dart_adso/ejeCondicionalesComplementarios/condicionales simples/test/condicionales_simples_test.dart
@@ -0,0 +1,8 @@
+import 'package:condicionales_simples/condicionales_simples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
