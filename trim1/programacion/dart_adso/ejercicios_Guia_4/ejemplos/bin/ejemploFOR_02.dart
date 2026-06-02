670aeb2edee7b8acf0b1aaa16f1516d5897f5c89
Angie Verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-06-02 12:04:43 -0500

    update

--- trim1/programacion/dart_adso/ejercicios_Guia_4/ejemplos/bin/ejemploFOR_02.dart
+++ trim1/programacion/dart_adso/ejercicios_Guia_4/ejemplos/bin/ejemploFOR_02.dart
@@ -1,13 +1,14 @@
-import 'dart:io';
-
+import 'dart:io'; 
 void main(List<String> args) {
-  int sumaTotal = 0; 
-  for (int i = 1; i <= 10; i++) {
-    print("Ingrese el número $i:");
-    
-    int num = int.parse(stdin.readLineSync()!);
-    
-    sumaTotal += num; 
+  double numero;
+  int valorFinal = 10;
+  String entradaDatos;
+  double suma=0; // Variable de tipo acumulador
+  for (int i = 0; i < valorFinal; i++) {
+    print("Ingrese el valor del número ${i+1}");
+    entradaDatos = stdin.readLineSync()!;
+    numero = double.tryParse(entradaDatos) ?? 0;
+    suma = suma + numero;
   }
-  print("\nLa suma de los 10 números es: $sumaTotal");
+  print("La suma de los números es: $suma");
 }
