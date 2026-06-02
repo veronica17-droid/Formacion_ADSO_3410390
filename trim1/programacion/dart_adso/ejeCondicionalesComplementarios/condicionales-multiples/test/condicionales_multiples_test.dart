91525920641ff341c6ca0a32b97fe2955681dafb
Angie Verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-06-02 09:15:01 -0500

    creacion carpeta condicionales multiples

---
+++ trim1/programacion/dart_adso/ejeCondicionalesComplementarios/condicionales-multiples/test/condicionales_multiples_test.dart
@@ -0,0 +1,8 @@
+import 'package:condicionales_multiples/condicionales_multiples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
