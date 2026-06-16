9213802fe41bff07efe06739c8a67695eb163d73
Angie verónica Carvajal <veronicacarvajalcardona17@gmail.com>
2026-06-16 12:27:36 -0500

    Creación carpeta ejercicios_for

---
+++ trim1/programacion/dart_adso/ejercicios_Guia_4/ejercicios_for/bin/ejercicios_for.dart
@@ -0,0 +1,5 @@
+import 'package:ejercicios_for/ejercicios_for.dart' as ejercicios_for;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ejercicios_for.calculate()}!');
+}
---
+++ trim1/programacion/dart_adso/ejercicios_Guia_4/ejercicios_for/test/ejercicios_for_test.dart
@@ -0,0 +1,8 @@
+import 'package:ejercicios_for/ejercicios_for.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
